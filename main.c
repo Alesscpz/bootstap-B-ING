c5e8848f075bafd1f868e89042c086e9ddf92df8
Alexandre <alexandre.bourse@epitech.eu>
2025-02-24 14:22:43 +0100

    [ADD] error check

--- main.c
+++ main.c
@@ -5,7 +5,19 @@
 ** main.c
 */
 
+#include <stddef.h>
+#include <stdlib.h>
+
+int error_check(int ac, char **env)
+{
+    if (ac != 1 || env[0] == NULL)
+        return -1;
+    return 0;
+}
+
 int main(int argc, char **argv, char **env)
 {
+    if (error_check(argc, env) == -1)
+        exit(84);
     return 0;
 }
