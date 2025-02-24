67c6689bea386388b6d46426174f9c2bd3b318f2
Alessandro <alessandro.capozzi@epitech.eu>
2025-02-24 14:17:53 +0100

    feat: add the function of the subject

---
+++ include/bs_world.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** bs_world
+** File description:
+** bs_world.h
+*/
+
+#ifndef BS_WORLD_H
+    #define BS_WORLD_H
+
+    #include <SFML/Graphics.h>
+
+sfVertexArray * create_line(sfVector2f *point1, sfVector2f *point2);
+
+#endif BS_WORLD_H
