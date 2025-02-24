32bfe930403a6ad955b64629db4453a70c6afff1
Côme Fougère <come.fougere@epitech.eu>
2025-02-24 14:16:40 +0100

    feat: convert degre in radian

---
+++ include/pi.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2025
+** main.c
+** File description:
+** main.c
+*/
+
+#ifndef PI_H_
+    #define PI_H_
+    #define M_PI 3.141592653589793238462643383279502884197169399375105820974944
+#endif
---
+++ src/convert_deg_rad.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** main.c
+** File description:
+** main.c
+*/
+
+#include "pi.h"
+
+double convert_degree_to_radian(double angle)
+{
+    return angle * M_PI / 180;
+}
